2f1a841bb3a9c8513275e596370439692dfd18c7
emufan4568 <geoster3d@gmail.com>
2022-08-28 15:42:37 +0300

    Update build script for Qt package
    * Also add MSVC Qt binaries. The current package has been compiled with
      clang-cl, which while more optimized, might not work for everyone.

--- qt/build-qt.cmd
+++ qt/build-qt.cmd
@@ -1,14 +1,17 @@
-C:\Qt\5.5\Src\configure -prefix %CD%\output ^
-        -debug-and-release -opensource -opengl desktop -mp -c++11 -ltcg ^
-        -nomake examples ^
-        -skip qtdeclarative -skip qtdoc -skip qtmultimedia ^
-        -skip qttools -skip qtwebkit -skip qtwebkit-examples ^
-        -skip qtxmlpatterns
+..\configure.bat -prefix %CD%\output -debug-and-release -opensource -opengl desktop -mp -ltcg ^
+        -nomake examples -nomake tests ^
+        -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcharts -skip qtconnectivity -skip qtdatavis3d ^
+        -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects -skip qtlocation -skip qtlottie -skip qtmacextras ^
+        -skip qtnetworkauth -skip qtpurchasing -skip qtquick3d -skip qtquickcontrols -skip qtquicktimeline ^
+        -skip qtremoteobjects -skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport ^
+        -skip qtspeech -skip qtsvg -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebsockets -skip qtwebview ^
+        -skip qtx11extras -skip qtxmlpatterns -skip qtvirtualkeyboard -skip qttools -skip qtquickcontrols2 -skip qtdeclarative ^
+        -skip qtnetworkauth -skip qtwinextras -skip qtimageformats
 
 nmake
 nmake install
 
-del output\bin\*.pdb
-del output\lib\*.pdb
-REM This library is huge and doesn't seem to be required to compile applications
-del output\lib\Qt5Bootstrap.*
+cd output
+del *.pdb /a /s
+del *.pdb /a /s
+rmdir /s /q doc
